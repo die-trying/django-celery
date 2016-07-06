1fff5a5aafc9e38ab2c217fd8e5e0f425d012341
Fedor Borshev <f@f213.in>
2016-07-06 18:50:58 +0300

    URL namespacing

--- elk/urls.py
+++ elk/urls.py
@@ -5,10 +5,17 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
+"""
+URL of every app should be namespaced with the prefix of the app name,
+in example namespace for buy_single url in the hub app is hub:buy_single,
+and for starting social auth — app:social:begin.
+"""
+
 urlpatterns = [
     url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
 
-    url(r'^accounts/', include('acc.urls')),
+    url(r'^accounts/', include('acc.urls', namespace='acc')),
+    url(r'^hub/', include('hub.urls', namespace='hub')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
--- hub/views.py
+++ hub/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+
+def single(request):
+    pass
