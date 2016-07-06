aeae51a8855f3a67fb019a585b93f59aad91f58e
Fedor Borshev <f@f213.in>
2016-07-06 14:53:09 +0300

    Frontend: plugin for stylus — nib

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var nib = require('nib')
 
 gulp.task('css', function(){
     return gulp.src('*/assets/css/*.styl')
         .pipe($.sourcemaps.init())
         .pipe($.concat('elk.styl'))
-        .pipe($.stylus())
+        .pipe($.stylus({
+            use: [nib()]
+        }))
         .pipe($.sourcemaps.write())
         .pipe(gulp.dest('./elk/static/css/'));
 });
