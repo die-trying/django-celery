0dbc064e0ed03d21370049d145bd8f7f42b10c32
Fedor Borshev <f@f213.in>
2016-07-06 20:02:39 +0300

    Generic single lesson buy process, refs #4

--- hub/urls.py
+++ hub/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'/buy-single', views.single, name='single')
+    url(r'^buy-single$', views.single, name='buy_a_single')
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,5 +1,17 @@
-#from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from moneyed import Money
+
+from hub.models import Class
+from lessons.models import OrdinaryLesson
 
 
+@login_required
 def single(request):
-    pass
+    c = Class(
+        customer=request.user.crm,
+        lesson=OrdinaryLesson.get_default(),
+        buy_price=Money(10, 'USD'),
+    )
+    c.save()
+    return redirect('/')
