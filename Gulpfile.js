25f8c2280d337e83572b26ce2a3eb2bd31048a93
Fedor Borshev <f@f213.in>
2016-07-07 22:03:26 +0300

    Frontend: single vendor.css file

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var nib = require('nib')
 
+var vendor_dir = './elk/static/vendor/'
+
 gulp.task('css', function(){
     return gulp.src('*/assets/css/*.styl')
         .pipe($.sourcemaps.init())
@@ -23,9 +25,7 @@ gulp.task('js', function(){
 });
 
 gulp.task('js:vendor', function(){
-    var files = require('./build/js-vendor-files.json');
-    var vendor_dir = './elk/static/vendor/'
-    files = files.map(function(f){
+    var files = require('./build/js-vendor-files.json').map(function(f){
         f = vendor_dir + f
         f = f.replace('//', '/');
         return f;
@@ -35,9 +35,21 @@ gulp.task('js:vendor', function(){
         .pipe(gulp.dest('./elk/static/js'));
 });
 
-gulp.task('default', ['js:vendor'], function(){
+gulp.task('css:vendor', function(){
+    var files = require('./build/css-vendor-files.json').map(function(f){
+        f = vendor_dir + f;
+        f = f.replace('//', '/');
+        return f;
+    })
+
+    return gulp.src(files)
+        .pipe($.concat('vendor.css'))
+        .pipe(gulp.dest('./elk/static/css'));
+});
+
+gulp.task('default', ['js:vendor', 'css:vendor'], function(){
     gulp.watch('*/assets/css/*.styl', ['css']);
     gulp.watch('*/assets/js/*.coffee', ['js']);
 });
 
-gulp.task('production', ['js', 'css', 'js:vendor']);
+gulp.task('production', ['js', 'css', 'js:vendor', 'css:vendor']);
