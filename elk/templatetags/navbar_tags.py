f49739d6faf63cf2be45dab0df1b802054dbf213
Fedor Borshev <f@f213.in>
2016-07-07 13:28:08 +0300

    Removed debug from navbar template

--- elk/templatetags/navbar_tags.py
+++ elk/templatetags/navbar_tags.py
@@ -7,7 +7,6 @@ register = template.Library()
 
 @register.simple_tag
 def is_active(request, pattern):
-    print(request.path)
     if re.search(pattern, request.path):
         return 'active'
     return ''
