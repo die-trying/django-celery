da3b27da65903f3fb9a36d6d12e232e7bd77b679
Fedor Borshev <f@f213.in>
2016-07-16 13:10:19 +0300

    migrations merge for the previous commit, refs #18

--- timeline/migrations/0001_initial.py
+++ timeline/migrations/0001_initial.py
@@ -1,52 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
 import django.db.models.deletion
 from django.conf import settings
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
+    # replaces = [('timeline', '0001_initial'), ('timeline', '0002_auto_20160701_2213'), ('timeline', '0003_auto_20160702_0735'), ('timeline', '0004_entry_duration'), ('timeline', '0005_auto_20160713_1548'), ('timeline', '0006_auto_20160713_1622'), ('timeline', '0007_auto_20160714_1053'), ('timeline', '0008_entry_active'), ('timeline', '0009_auto_20160716_0413')]
+
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('crm', '0002_auto_20160701_0448'),
         ('contenttypes', '0002_remove_content_type_name'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Entry',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
-                ('start_time', models.DateTimeField()),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('start', models.DateTimeField()),
+                ('end', models.DateTimeField()),
                 ('slots', models.SmallIntegerField(default=1)),
-                ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True, related_name='timeline_entries', blank=True)),
-                ('customers', models.ManyToManyField(to='crm.Customer', related_name='planned_timeline_entries', blank=True))
+                ('taken_slots', models.SmallIntegerField(default=0)),
+                ('lesson_id', models.PositiveIntegerField(blank=True, null=True)),
+                ('lesson_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='timeline_entries', to=settings.AUTH_USER_MODEL)),
+                ('customers', models.ManyToManyField(blank=True, related_name='planned_timeline_entries', to='crm.Customer')),
+                ('active', models.SmallIntegerField(choices=[(0, 'Inactive'), (1, 'Active')], default=1)),
             ],
         ),
         migrations.AlterModelOptions(
             name='entry',
             options={'verbose_name_plural': 'Entries'},
         ),
-        migrations.AddField(
-            model_name='entry',
-            name='event_id',
-            field=models.PositiveIntegerField(blank=True, null=True),
-        ),
-        migrations.AddField(
-            model_name='entry',
-            name='event_type',
-            field=models.ForeignKey(to='contenttypes.ContentType', null=True, blank=True),
-        ),
-        migrations.AlterField(
-            model_name='entry',
-            name='teacher',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL, related_name='timeline_entries'),
-        ),
-        migrations.AddField(
-            model_name='entry',
-            name='taken_slots',
-            field=models.SmallIntegerField(default=0),
+        migrations.AlterModelOptions(
+            name='entry',
+            options={'permissions': (('other_entries', "Can work with other's timeleine entries"),), 'verbose_name_plural': 'Entries'},
         ),
     ]
