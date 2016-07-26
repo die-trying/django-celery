cc51398a2e0aadfd9b1a70025081653e65ce6e54
Fedor Borshev <f@f213.in>
2016-07-26 21:44:15 +0300

    build fix, refs #37

--- build/store-build-information.sh
+++ build/store-build-information.sh
@@ -4,8 +4,8 @@ git rev-parse HEAD > revision.txt
 
 F='./elk/static/build.txt'
 
-echo "branch: $CIRCLE_BRANCH" > F
-echo "commit: $CIRCLE_SHA1" >> F
-echo "build   $CIRCLE_BUILD_NUM" >> F
+echo "branch: $CIRCLE_BRANCH" > $F
+echo "commit: $CIRCLE_SHA1" >> $F
+echo "build   $CIRCLE_BUILD_NUM" >> $F
 
 
