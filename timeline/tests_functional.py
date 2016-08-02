f10b6ad9cb4fd1a951d73b0e50e7e1610a462e05
Fedor Borshev <f@f213.in>
2016-08-02 21:19:12 +0300

    Minor timeline tests relocation

--- timeline/tests_functional.py
+++ timeline/tests_functional.py
@@ -7,15 +7,17 @@ from mixer.backend.django import mixer
 
 import lessons.models as lessons
 from elk.utils.testing import ClientTestCase, create_teacher
-from teachers.models import WorkingHours
 from timeline.models import Entry as TimelineEntry
 
 
 class EntryCRUDTest(ClientTestCase):
+    """
+    Test hand-crafted form for event editing throught the calendar.
+    """
     def setUp(self):
         self.teacher = create_teacher()
-        self.lesson = mixer.blend(lessons.OrdinaryLesson, duration=timedelta(minutes=33))
-        self.lesson_type = ContentType.objects.get_for_model(lessons.OrdinaryLesson).pk
+        self.lesson = mixer.blend(lessons.MasterClass, host=self.teacher, duration=timedelta(minutes=33))
+        self.lesson_type = ContentType.objects.get_for_model(lessons.MasterClass).pk
 
         super().setUp()
 
@@ -137,61 +139,3 @@ class EntryAPITest(ClientTestCase):
 
         data = json.loads(response.content.decode('utf-8'))
         self.assertEqual(len(data), 3)
-
-
-class TestFormAPIHelpers(ClientTestCase):
-    def setUp(self):
-        self.teacher = create_teacher()
-        self.lesson = mixer.blend(lessons.MasterClass, host=self.teacher)
-        self.entry = TimelineEntry(
-            teacher=self.teacher,
-            lesson=self.lesson,
-            start=datetime(2016, 1, 18, 14, 10),
-            end=datetime(2016, 1, 18, 14, 40),
-            allow_overlap=False,
-        )
-
-        mixer.blend(WorkingHours, teacher=self.teacher, weekday=0, start='13:00', end='15:00')
-
-        self.entry.save()
-        super().setUp()
-
-    def test_check_overlap_true(self):
-        overlaps = self.__check_entry(
-            username=self.teacher.user.username,
-            start='2016-01-18 14:30',
-            end='2016-01-18 15:00',
-        )
-        self.assertTrue(overlaps['is_overlapping'])
-
-    def test_check_overlap_false(self):
-        not_overlaps = self.__check_entry(
-            username=self.teacher.user.username,
-            start='2016-01-18 14:45',
-            end='2016-01-18 15:15'
-        )
-        self.assertFalse(not_overlaps['is_overlapping'])
-
-    def test_check_hours_true(self):
-        fits = self.__check_entry(
-            username=self.teacher.user.username,
-            start='2032-05-03 14:00',  # monday
-            end='2032-05-03 14:30',
-        )
-        self.assertTrue(fits['is_fitting_hours'])
-
-    def test_check_hours_false(self):
-        fits = self.__check_entry(
-            username=self.teacher.user.username,
-            start='2032-05-03 14:00',  # monday
-            end='2032-05-03 15:30',  # half-hour late
-        )
-        self.assertFalse(fits['is_fitting_hours'])
-
-    def __check_entry(self, username, start, end):
-        response = self.c.get(
-            '/timeline/%s/check_entry/%s/%s/' % (username, start, end)
-        )
-        self.assertEqual(response.status_code, 200)
-        result = json.loads(response.content.decode('utf-8'))
-        return result
--- timeline/tests_unit.py
+++ timeline/tests_unit.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
@@ -278,7 +280,7 @@ class TestPermissions(ClientTestCase):
 class TestCancel(TestCase):
     """
     Actions with timeline entries with attached classes should
-    influence the classes too.
+    influence the classes too, and vice-versa
     """
     def setUp(self):
         self.teacher = create_teacher()
@@ -304,7 +306,7 @@ class TestCancel(TestCase):
 
     def test_cancel_entry(self):
         """
-        When deleting a timeline entry class should become unscheduled.
+        When deleting a timeline entry, the class should become unscheduled.
         """
         c = self._buy_a_lesson(self.lesson)
         entry = self._create_entry()
@@ -383,3 +385,65 @@ class TestFormContext(ClientTestCase):
         response = self.c.get('/timeline/%s/%d/update/' % (self.teacher.user.username, entry.pk))
         with self.assertHTML(response, 'form.form #teacher') as (input,):
             self.assertEquals(input.value, str(self.teacher.pk))
+
+
+class TestCheckEntry(ClientTestCase):
+    """
+    :view:`timeline.check_entry` is a helper for the timeline creating form
+    which checks entry validity — working hours and overlaping
+    """
+    def setUp(self):
+        self.teacher = create_teacher()
+        self.lesson = mixer.blend(lessons.MasterClass, host=self.teacher)
+        self.entry = TimelineEntry(
+            teacher=self.teacher,
+            lesson=self.lesson,
+            start=datetime(2016, 1, 18, 14, 10),
+            end=datetime(2016, 1, 18, 14, 40),
+            allow_overlap=False,
+        )
+
+        mixer.blend(WorkingHours, teacher=self.teacher, weekday=0, start='13:00', end='15:00')
+
+        self.entry.save()
+        super().setUp()
+
+    def test_check_overlap_true(self):
+        overlaps = self.__check_entry(
+            username=self.teacher.user.username,
+            start='2016-01-18 14:30',
+            end='2016-01-18 15:00',
+        )
+        self.assertTrue(overlaps['is_overlapping'])
+
+    def test_check_overlap_false(self):
+        not_overlaps = self.__check_entry(
+            username=self.teacher.user.username,
+            start='2016-01-18 14:45',
+            end='2016-01-18 15:15'
+        )
+        self.assertFalse(not_overlaps['is_overlapping'])
+
+    def test_check_hours_true(self):
+        fits = self.__check_entry(
+            username=self.teacher.user.username,
+            start='2032-05-03 14:00',  # monday
+            end='2032-05-03 14:30',
+        )
+        self.assertTrue(fits['is_fitting_hours'])
+
+    def test_check_hours_false(self):
+        fits = self.__check_entry(
+            username=self.teacher.user.username,
+            start='2032-05-03 14:00',  # monday
+            end='2032-05-03 15:30',  # half-hour late
+        )
+        self.assertFalse(fits['is_fitting_hours'])
+
+    def __check_entry(self, username, start, end):
+        response = self.c.get(
+            '/timeline/%s/check_entry/%s/%s/' % (username, start, end)
+        )
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content.decode('utf-8'))
+        return result
