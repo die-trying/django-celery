d967bf953d678055548735d804726ccdd4ca6f47
Fedor Borshev <f@f213.in>
2016-08-04 21:49:55 +0300

    Now geoip files will be downloaded during integration only, if they are newer

--- build/download_geoip_db.sh
+++ build/download_geoip_db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd geolite
-wget -c http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-wget -c http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
+wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
 
 gunzip -kqf GeoLiteCity.dat.gz
 gunzip -kqf GeoLiteCityv6.dat.gz
