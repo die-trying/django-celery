87e4f88d9a140f77178fa9ae6cc16196af1bbdab
Fedor Borshev <f@f213.in>
2016-08-09 16:11:18 +0300

    Enabled profiling in the debug toolbar

--- elk/settings.py
+++ elk/settings.py
@@ -136,6 +136,21 @@ SOCIAL_AUTH_PIPELINE = (
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '/static/vendor/jquery/dist/jquery.min.js',
 }
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
 
 INTERNAL_IPS = [
     '127.0.0.1',
