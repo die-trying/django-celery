6fc3db7bfa58638135207081feadcbd7a28fca94
Fedor Borshev <f@f213.in>
2016-08-09 22:05:37 +0300

    Temporary disabled product admin — it is not needed yet

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-
-from elk.utils.admin import ModelAdmin
-
-from .models import Product1
-
-
-# Register your models here.
-
-@admin.register(Product1)
-class Product1Admin(ModelAdmin):
-    pass
+# from django.contrib import admin
+#
+# from elk.utils.admin import ModelAdmin
+#
+# from .models import Product1
+#
+#
+# # Register your models here.
+#
+# @admin.register(Product1)
+# class Product1Admin(ModelAdmin):
+#     pass
