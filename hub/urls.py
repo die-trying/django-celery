f94af02aeef7c92c35dfd3e4fdb8c95ebbc92e64
Fedor Borshev <f@f213.in>
2016-08-15 21:15:01 +0300

    Removed stale service views, closes #97

--- hub/urls.py
+++ hub/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^buy-single$', views.single, name='buy_a_single'),
-    url(r'^buy-subscription$', views.subscription, name='buy_a_subscription'),
-
     url(r'(?P<date>[\d\-]+)/type/(?P<lesson_type>\d+)/teachers.json$', views.teachers, name='teachers'),
     url(r'(?P<date>[\d\-]+)/type/(?P<lesson_type>\d+)/lessons.json$', views.lessons, name='lessons'),
 
--- hub/views.py
+++ hub/views.py
@@ -1,37 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from moneyed import Money
 
-from hub.models import Class, Subscription
 from hub.sortinghat import SortingHat
-from lessons.models import OrdinaryLesson
-from products.models import Product1
 from teachers.models import Teacher
-
-
-@login_required
-def single(request):
-    c = Class(
-        customer=request.user.crm,
-        lesson=OrdinaryLesson.get_default(),
-        buy_price=Money(10, 'USD'),
-    )
-    c.request = request
-    c.save()
-    return redirect('/')
-
-
-@login_required
-def subscription(request):
-    s = Subscription(
-        customer=request.user.crm,
-        product=Product1.objects.get(pk=1),
-        buy_price=Money(150, 'USD')
-    )
-    s.request = request
-    s.save()
-    return redirect('/')
 
 
 @login_required
