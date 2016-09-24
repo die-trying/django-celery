045f6c7fb1b925ffe02a8cc4921ebc484aabedbb
Fedor Borshev <f@f213.in>
2016-09-24 21:02:08 +0300

    Removed unused manual completion log from teacher profile — save CPU time

--- teachers/admin/teachers.py
+++ teachers/admin/teachers.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.db import models
-from django.template.defaultfilters import capfirst
 from suit.widgets import HTML5Input
 
 from elk.admin import ModelAdmin, StackedInline, TabularInline
 from extevents.models import ExternalEvent, GoogleCalendar
-from manual_class_logging.models import ManualClassLogEntry
 from teachers.models import Teacher, WorkingHours
 
 
@@ -37,34 +35,11 @@ class GooogleCalendarInline(TabularInline):
         }
 
 
-class ManualClassLogEntriesInline(TabularInline):
-    model = ManualClassLogEntry
-    readonly_fields = ('customer', 'lesson_type', 'completion_time')
-    fieldsets = (
-        (None, {'fields': ('customer', 'lesson_type', 'completion_time')}),
-    )
-
-    def customer(self, instance):
-        return instance.Class.customer.full_name
-
-    def lesson_type(self, instance):
-        return capfirst(instance.Class.lesson_type.model_class()._meta.verbose_name)
-
-    def completion_time(self, instance):
-        return self._datetime(instance.timestamp)
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-
 @admin.register(Teacher)
 class TeacherAdmin(ModelAdmin):
     list_display = ('__str__', 'manualy_completed_classes', 'lessons_allowed')
 
-    inlines = (ManualClassLogEntriesInline, WorkingHoursInline, GooogleCalendarInline)
+    inlines = (WorkingHoursInline, GooogleCalendarInline)
 
     def manualy_completed_classes(self, instance):
         return instance.manualy_completed_classes.count()
