1b9d672e814ec49fc67c2f09723c573d6a5cbd42
Fedor Borshev <f@f213.in>
2016-09-24 20:59:22 +0300

    Now sentry by default logs all exception info

--- elk/logging.py
+++ elk/logging.py
@@ -8,8 +8,10 @@ def _get_logger():
 class logger():
     @staticmethod
     def warning(*args, **kwargs):
+        kwargs['exc_info'] = True
         _get_logger().warning(*args, **kwargs)
 
     @staticmethod
     def error(*args, **kwargs):
+        kwargs['exc_info'] = True
         _get_logger().error(*args, **kwargs)
