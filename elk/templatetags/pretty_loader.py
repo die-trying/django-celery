910d5c346936bec63a7b19f0cffe520f713cf7b3
Fedor Borshev <f@f213.in>
2016-10-07 21:07:56 +0300

    Moved pretty loaded to the CSS

--- elk/templatetags/pretty_loader.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.templatetags.static import static
-
-register = template.Library()
-
-
-@register.simple_tag
-def pretty_loader():
-    return 'style="background-image: url(\'%s\')"' % static('i/loader.svg')
