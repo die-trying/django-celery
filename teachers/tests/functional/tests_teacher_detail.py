2e3c00589bbd8bef46f22db07c8baf28000234b3
Fedor Borshev <f@f213.in>
2016-10-26 22:37:08 +0300

    Teacher detail draft markup, refs #164

--- teachers/tests/functional/tests_teacher_detail.py
+++ teachers/tests/functional/tests_teacher_detail.py
@@ -1,18 +1,27 @@
 from elk.utils.testing import ClientTestCase, create_customer, create_teacher
+from teachers.slot_list import SlotList
 
 
 class TestTeacherDetail(ClientTestCase):
     def setUp(self):
-        self.teacher = create_teacher()
+        self.teacher = create_teacher(works_24x7=True)
 
-    def test_card_loading(self):
+    def test_loading(self):
         response = self.c.get('/teachers/%s/' % self.teacher.user.username)
 
         with self.assertHTML(response, 'img.teacher-face') as teacher_face:
             self.assertIsNotNone(teacher_face)
 
-    def test_no_teacher_card_for_non_teacher_user(self):
+    def test_no_teacher_detail_for_non_teacher_user(self):
         some_random_man = create_customer()
         response = self.c.get('/teachers/%s/' % some_random_man.user.username)
 
         self.assertEqual(response.status_code, 404)
+
+    def test_teacher_detail_slots_list(self):
+        response = self.c.get('/teachers/%s/' % self.teacher.user.username)
+        self.assertGreaterEqual(len(response.context['timeslots']), 7)
+
+        first_day = response.context['timeslots'][0]
+        self.assertIsInstance(first_day['slots'], SlotList)
+        self.assertGreaterEqual(len(first_day['slots']), 10)
