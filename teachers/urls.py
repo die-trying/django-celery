642831a1e93f44790c214c6695d55d2fa94580da
Fedor Borshev <f@f213.in>
2016-10-28 19:52:43 +0300

    Removed unused working hours API endpoint

--- teachers/urls.py
+++ teachers/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'(?P<username>.+)/hours.json$', views.hours, name='hours'),
     url(r'(?P<username>.+)/', views.TeacherDetail.as_view()),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -1,23 +1,8 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-from django.shortcuts import get_list_or_404, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 
-from teachers.models import Teacher, WorkingHours
-
-
-@staff_member_required
-def hours(request, username):
-    teacher = get_object_or_404(Teacher, user__username=username)
-
-    hours_list = []
-
-    for hours in get_list_or_404(WorkingHours, teacher=teacher):
-        hours_list.append(hours.as_dict())
-
-    return JsonResponse(hours_list, safe=False)
+from teachers.models import Teacher
 
 
 class TeacherDetail(DetailView):
