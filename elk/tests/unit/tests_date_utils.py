1a5c3f2d81f140109f60562475cb634b5bc19296
Fedor Borshev <f@f213.in>
2016-11-03 21:45:19 +0300

    Code cleanup: removed 2 unused methods

--- elk/tests/unit/tests_date_utils.py
+++ elk/tests/unit/tests_date_utils.py
@@ -1,27 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
-from django.utils.dateformat import format
-from django.utils.dateparse import parse_date
-
-from elk.utils.date import ago, common_timezones, day_range, fwd
+from elk.utils.date import common_timezones, day_range
 from elk.utils.testing import TestCase
 
 
 class TestDateUtils(TestCase):
-    def test_ago(self):
-        d = parse_date('2014-03-29')
-        self.assertEqual(ago(d, days=29), '2014-02-28')
-        self.assertEqual(ago(d, days=29, fmt='m/d/Y'), '02/28/2014')
-
-        self.assertEqual(ago(days=16), format(datetime.now() - timedelta(days=16), 'Y-m-d'))
-
-    def test_fwd(self):
-        d = parse_date('2014-02-28')
-        self.assertEqual(fwd(d, days=29), '2014-03-29')
-        self.assertEqual(fwd(d, days=29, fmt='m/d/Y'), '03/29/2014')
-
-        self.assertEqual(fwd(days=16), format(datetime.now() + timedelta(days=16), 'Y-m-d'))
-
     def test_day_range(self):
         r = day_range('2016-02-28')
         self.assertEquals(r, ('2016-02-28 00:00:00', '2016-02-28 23:59:59'))
--- elk/utils/date.py
+++ elk/utils/date.py
@@ -2,11 +2,9 @@
     Shortcuts for dealing with timedelta in format, unsderstandable by django
     models.
 """
-from copy import copy
 from datetime import date, datetime, time, timedelta
 
 import pytz
-from django.utils.dateformat import format
 
 
 def minute_till_midnight(date):
@@ -15,24 +13,6 @@ def minute_till_midnight(date):
 
 def minute_after_midnight(date):
     return datetime.combine(date + timedelta(days=1), time.min)
-
-
-def ago(date=datetime.now(), fmt='Y-m-d', **kwargs):
-    """
-    Move date backwards. Arguments are compatible with `timedelta<https://docs.python.org/3/library/datetime.html#datetime.timedelta>`_
-    """
-    d = copy(date)
-    d -= timedelta(**kwargs)
-    return format(d, fmt)
-
-
-def fwd(date=datetime.now(), fmt='Y-m-d', **kwargs):
-    """
-    Move date forward. Arguments are compatible with `timedelta<https://docs.python.org/3/library/datetime.html#datetime.timedelta>`_
-    """
-    d = copy(date)
-    d += timedelta(**kwargs)
-    return format(d, fmt)
 
 
 def day_range(d):
