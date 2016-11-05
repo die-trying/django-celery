76c8c2346dfa8bba61ebc05cf2177763658da6f6
Fedor Borshev <f@f213.in>
2016-11-05 15:47:08 +0300

    codestyle fix

--- elk/urls.py
+++ elk/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
 from rest_framework.routers import DefaultRouter
 
 from acc.views import Homepage
@@ -16,12 +15,13 @@ and for starting social auth — app:social:begin.
 """
 
 
-router = DefaultRouter()
-router.register(r'teachers', TeacherViewSet)
-router.register(r'timeline', TimelineViewset)
+api_router = DefaultRouter()
+api_router.register(r'teachers', TeacherViewSet)
+api_router.register(r'timeline', TimelineViewset)
 
 urlpatterns = [
     url(name='home', regex=r'^$', view=Homepage.as_view()),
+
     url(r'^accounts/', include('acc.urls', namespace='acc')),
     url(r'^crm/', include('crm.urls', namespace='crm')),
     url(r'^market/', include('market.urls', namespace='market')),
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^teachers/', include('teachers.urls', namespace='teachers')),
     url(r'^payments/', include('payments.urls', namespace='payments')),
 
-    url(r'^api/', include(router.urls, namespace='api')),
+    url(r'^api/', include(api_router.urls, namespace='api')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
